52bbb4e01c21915b39a3a6538e637b230a93c461
Roberto VP <roberto.valladarespiedras@gmail.com>
2016-05-25 14:28:31 -0700

    hello5

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,16 +15,15 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => "http://52.39.23.168/" }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :user_name            => ENV["GMAIL_USERNAME"],
-    :password             => ENV["GMAIL_PASSWORD"],
+    :address              => "smtp.mail.yahoo.com",
+    :port                 => 465,
+    :user_name            => ENV["YAHOO_USERNAME"],
+    :password             => ENV["YAHOO_PASSWORD"],
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,10 +79,10 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-   :address              => "smtp.gmail.com",
-   :port                 => 587,
-   :user_name            => ENV['GMAIL_USERNAME'],
-   :password             => ENV['GMAIL_PASSWORD'],
+   :address              => "smtp.mail.yahoo.com",
+   :port                 => 465,
+   :user_name            => ENV['YAHOO_USERNAME'],
+   :password             => ENV['YAHOO_PASSWORD'],
    :authentication       => "plain",
   :enable_starttls_auto => true
   }
