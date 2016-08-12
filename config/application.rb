ecdf503d589822c917d402845368313a0f00bd1f
Roberto VP <roberto.valladarespiedras@gmail.com>
2016-08-12 12:45:49 -0500

    without_https

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,6 @@ Bundler.require(*Rails.groups)
 
 module Primeraraiz5
   class Application < Rails::Application
-    config.force_ssl = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
