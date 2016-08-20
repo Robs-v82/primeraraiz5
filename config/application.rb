cd999b014dab937d38d7d53ac711d2ea68bfad4d
Roberto VP <roberto.valladarespiedras@gmail.com>
2016-08-20 11:29:15 -0500

    trying_to_force_thpps2

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,8 @@ module Primeraraiz5
     # -- all .rb files in that directory are automatically loaded.
     Conekta.api_key = Rails.application.secrets.conekta_private 
     
+    config.force_ssl = true
+
     config.action_mailer.asset_host = "http://primeraraiz.com" 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
