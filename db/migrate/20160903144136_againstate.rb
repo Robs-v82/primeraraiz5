96d45556284818e31aa3cca9a22f13829b94b2a2
Roberto VP <roberto.valladarespiedras@gmail.com>
2016-09-03 09:56:31 -0500

    kjhlkjh

--- db/migrate/20160903144136_againstate.rb
+++ db/migrate/20160903144136_againstate.rb
@@ -1,5 +1,6 @@
 class Againstate < ActiveRecord::Migration
   def change
+  	remove_column :contacts, :state
   	add_column :contacts, :state, :string
   	change_column_default(:contacts, :state, "Ciudad de México")
   end
