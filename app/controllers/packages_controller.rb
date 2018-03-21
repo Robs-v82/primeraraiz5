4f47e79a1d4835b1ab9454f8904fdc22f0ad6965
Roberto VP <roberto.valladarespiedras@gmail.com>
2018-03-20 21:37:40 -0600

    roof99

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -130,7 +130,8 @@ class PackagesController < ApplicationController
     html = render_to_string(:layout => "/myQuote", :layout => false)
     puts html
     kit = PDFKit.new(html)
-    kit = kit.to_file('https://primeraraiz.com/assets/'+session[:fileName]+'.pdf')
+    FileUtils.makedirs('Quotes/'+session[:fileName]+'.pdf')
+    kit = kit.to_file('Quotes/'+session[:fileName]+'.pdf')
     myPackage = Package.last
     myPackage.update(:docs=>kit)
     myFile = myPackage.docs
